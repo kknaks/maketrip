58738019a811d9b886e9056d02f99d96e6b72825
kknaks <benesia93@naver.com>
2024-11-09 16:54:22 +0900

    .

--- app/src/main/java/project/tripMaker/user/UserRole.java
+++
@@ -1,5 +0,0 @@
-package project.tripMaker.user;
-
-public enum UserRole {
-  ROLE_ADMIN, ROLE_USER
-}
--- app/src/main/java/project/tripMaker/vo/State.java
+++
@@ -1,9 +0,0 @@
-package project.tripMaker.vo;
-
-import lombok.Data;
-
-@Data
-public class State {
-  private String stateCode;
-  private String stateName;
-}
